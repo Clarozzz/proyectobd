60d3a5c60feb174a0a9549e0c1bd67800c4e163b
Cesar <jcsdlg49@gmail.com>
2023-04-21 21:07:47 -0600

    Agregado vistas

---
+++ resources/views/empleados/index.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scam</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body class="bg-body-tertiary">
+
+    <nav id="navbar-inicio" class="navbar sticky-top bg-primary text-white">
+        <div class="container justify-content-center">
+            <div class="d-flex">
+                <img src="{{URL('images/logo.png')}}" alt="Logo" height="55" class="d-inline-block align-text-top">
+                <h1 id="encabezado-navbar" class="m-0 align-self-center">SCAM</h1>
+            </div>
+        </div>
+    </nav>
+
+    <main id="paginaPrincipal" class="my-5">
+        <h1 class="text-center mb-5">Administrar</h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col px-5">
+                    <div class="card mb-3">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Motoristas</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col px-5">
+                    <div class="card mb-3">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Solicitudes</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col px-5">
+                    <div class="card mb-3">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Clientes</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*------------- 
+
+    Clientes 
+
+-------------*/
+
+Route::get('/clientes', function () {
+    return view('clientes.inicio');
+})->name('clientes.inicio');
+
+Route::get('/clientes/registro', function () {
+    return view('clientes.registro');
+})->name('clientes.registro');
+
+Route::get('/clientes/solicitud', function () {
+    return view('clientes.solicitud');
+})->name('clientes.solicitud');
+
+Route::get('/clientes/factura', function () {
+    return view('clientes.factura');
+})->name('clientes.factura');
+
+/*------------- 
+
+    Motoristas 
+    
+-------------*/
+
+
+Route::get('/motoristas', function () {
+    return view('motoristas.inicio');
+})->name('motoristas.inicio');
+
+Route::get('/motoristas/registro', function () {
+    return view('motoristas.registro');
+})->name('motoristas.registro');
+
+Route::get('/motoristas/espera', function () {
+    return view('motoristas.espera');
+})->name('motoristas.espera');
+
+Route::get('/motoristas/solicitud', function () {
+    return view('motoristas.solicitud');
+})->name('motoristas.solicitud');
+
+
+/*------------- 
+
+    Empleados 
+    
+-------------*/
+
+
+Route::get('/empleados', function () {
+    return view('empleados.inicio');
+})->name('empleados.inicio');
+
+Route::get('/empleados/index', function () {
+    return view('empleados.index');
+})->name('empleados.index');
+
