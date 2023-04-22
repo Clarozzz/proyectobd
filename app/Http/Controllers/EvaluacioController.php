51f85cfa08bd6950c8af53d7c2b50995eb6a576c
andre <andre.herrera@unah.hn>
2023-04-22 18:56:58 -0500

    controladores y modelos avance

---
+++ app/Http/Controllers/EvaluacioController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Evaluacio;
+use Illuminate\Http\Request;
+
+class EvaluacioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Evaluacio $evaluacio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Evaluacio $evaluacio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Evaluacio $evaluacio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Evaluacio $evaluacio)
+    {
+        //
+    }
+}
