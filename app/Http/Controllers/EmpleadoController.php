9e4ced3bd2c1196fcd34ae3c84e1acfbf09e85e3
haru-chan25 <108935716+haru-chan25@users.noreply.github.com>
2023-04-25 16:27:27 -0600

    metodo store falta xd
    Falta aun

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use App\Models\Empleado;
+use App\Models\Motorista;
+use App\Models\Solicitud;
+use App\Models\Sucursal;
+use App\Models\Salario;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class EmpleadoController extends Controller
@@ -17,12 +23,20 @@ class EmpleadoController extends Controller
 
     public function indexClientes()
     {
-        return view('empleados.clientes');
+        $clientes = User::select()
+            ->join('cliente', 'cliente.idPersona', '=', 'persona.idPersona')
+            ->get();
+        return view('empleados.clientes', compact('clientes'));
     }
 
     public function indexSolicitudes()
     {
-        return view('empleados.solicitudes');
+        $solicitudes = Solicitud::select()
+            ->join('cliente', 'cliente.idCliente', '=', 'solicitud.idCliente')
+            ->join('valorImpuesto', 'valorImpuesto.idImpuesto', '=', 'solicitud.idImpuesto')
+            ->join('persona', 'persona.idPersona', '=', 'cliente.idPersona')
+            ->get();
+        return view('empleados.solicitudes', compact('solicitudes'));
     }
 
     public function indexDashboard()
@@ -32,12 +46,20 @@ class EmpleadoController extends Controller
 
     public function indexMotoristas()
     {
-        return view('empleados.motoristas');
+        $motoristas = Motorista::select()
+            ->join('persona', 'motorista.idPersona', '=', 'persona.idPersona')
+            ->get()
+            ->where('persona.estaHabilitado', false);
+        return view('empleados.motoristas', compact('motoristas'));
     }
 
     public function indexAsignarSolicitud()
     {
-        return view('empleados.asignarsolicitud');
+        $motoristas = Motorista::select()
+            ->join('persona', 'motorista.idPersona', '=', 'persona.idPersona')
+            ->get()
+            ->where('persona.estaHabilitado', true);
+        return view('empleados.asignarsolicitud', compact('motoristas'));
     }
 
     /**
@@ -53,8 +75,48 @@ class EmpleadoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = new User();
+        $user->primerNombre = $request->primerNombre;
+        $user->primerApellido = $request->primerApellido;
+        $user->segundoNombre = $request->segundoNombre;
+        $user->segundoApellido = $request->segundoApellido;
+        $user->telefono = $request->telefono;
+        $user->dni = $request->dni;
+        $user->rtn = $request->rtn;
+        $user->fechaNacimiento = $request->fechaNacimiento;
+        $user->email = $request->email;
+        $user->password = $request->contrasena;
+        $user->fechaAlta = Carbon::now();
+        $user->nombreEmpresa = $request->nombreEmpresa;
+        $user->estaHabilitado = true;
+
+        $user->save();
+
+        $id = User::select('idPersona')
+            ->where('dni', '=', $user->dni)
+            ->get();
+
+        $salario = new Salario();
+        $salario->mesPagado = $request->mesPagado;
+        $salario->salarioBruto = $request->salarioBruto;
+        $salario->salarioNeto = $request->salarioNeto;
+        $salario->fecha = Carbon::now();
+
+        $salario->save();
+
+        $idS = Salario::select('idSalario')
+        ->where('fecha', '=', $salario->fecha)
+        ->get();
+
+        $empleado = new Empleado();
+        $empleado->idSalario = $idS[0]->idSalario;
+        $empleado->idPersona = $id[0]->idPersona;
+
+        $empleado->save();
+
+        return redirect()->route('empleados.inicio');
     }
+
 
     /**
      * Display the specified resource.
